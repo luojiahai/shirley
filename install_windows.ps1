b4fcb310ef00304cd69539a3bf67fc2ab19664ff
luojiahai <ljiahai@hotmail.com>
2024-05-15 21:34:53 +1000

    Remove ps1

--- install_windows.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest https://www.python.org/ftp/python/3.10.6/python-3.10.6-embed-amd64.zip -OutFile .\tmp\python.zip
-Expand-Archive -Path .\tmp\python.zip -DestinationPath .\python_temp
-Remove-Item .\tmp\python.zip

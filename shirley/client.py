81a74928e8f3612fe067ed30e3370c5767247353
luojiahai <ljiahai@hotmail.com>
2024-07-27 09:51:17 +1000

    Update

--- shirley/client.py
+++ shirley/client.py
@@ -15,14 +15,7 @@ class Client(object):
                 f'Pretrained model not found in path {pretrained_model_path}.'
             )
 
-        if torch.cuda.is_available():
-            self._device = torch.device('cuda')
-        elif torch.backends.mps.is_available():
-            self._device = torch.device('mps')
-        else:
-            self._device = torch.device('cpu')
-
-        self._tokenizer = QWenTokenizer.from_pretrained(
+        tokenizer = QWenTokenizer.from_pretrained(
             pretrained_model_name_or_path=pretrained_model_path,
             local_files_only=True,
         )
@@ -37,25 +30,31 @@ class Client(object):
             local_files_only=True,
             trust_remote_code=True,
         )
-
         if model.generation_config.pad_token_id is not None:
             model.generation_config.pad_token_id = torch.tensor(
                 [model.generation_config.pad_token_id],
-                device=self._device,
+                device=self.device,
             )
         if model.generation_config.eos_token_id is not None:
             model.generation_config.eos_token_id = torch.tensor(
                 [model.generation_config.eos_token_id],
-                device=self._device,
+                device=self.device,
             )
 
-        self._model = model.to(device=self._device)
+        self._tokenizer = tokenizer
+        self._model = model.to(device=self.device)
 
         return
 
     @property
     def device(self) -> torch.device:
-        return self._device
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        elif torch.backends.mps.is_available():
+            device = torch.device('mps')
+        else:
+            device = torch.device('cpu')
+        return device
 
     @property
     def tokenizer(self) -> QWenTokenizer:
@@ -65,8 +64,6 @@ class Client(object):
     def model(self) -> QWenLMHeadModel:
         return self._model
 
-    @classmethod
     def generate(self, text: str, history: HistoryType = None) -> Tuple[str, HistoryType]:
-        chat = [{'text': text}]
-        query = self.tokenizer.from_list_format(chat)
+        query = self.tokenizer.from_list_format([{'text': text}])
         return self.model.chat(tokenizer=self.tokenizer, query=query, history=history)
--- shirley/webui.py
+++ shirley/webui.py
@@ -97,8 +97,7 @@ def generate(chatbot: Chatbot, task_history: TaskHistory) -> Iterator[Tuple[Chat
     history.append((augmented_query, full_response))
     image = CLIENT.tokenizer.draw_bbox_on_latest_picture(response=full_response, history=history)
     if image is not None:
-        image_temp_directory = secrets.token_hex(20)
-        image_temp_directory = Path(GRADIO_TEMP_DIRECTORY) / image_temp_directory
+        image_temp_directory = Path(GRADIO_TEMP_DIRECTORY) / secrets.token_hex(20)
         image_temp_directory.mkdir(exist_ok=True, parents=True)
         name = f'tmp{secrets.token_hex(5)}.jpg'
         filename = image_temp_directory / name

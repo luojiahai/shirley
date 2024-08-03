a14136a0a1a4d20591c156619484595d802f7c6a
luojiahai <ljiahai@hotmail.com>
2024-08-03 10:15:18 +1000

    Update

--- shirley/client.py
+++ shirley/client.py
@@ -1,12 +1,18 @@
+import logging
 import os
-import uuid
+import sys
 import torch
 import transformers
+import uuid
 from models.qwen_vl_chat.modeling_qwen import QWenLMHeadModel
 from models.qwen_vl_chat.tokenization_qwen import QWenTokenizer
 from models.qwen_vl_chat.qwen_generation_utils import HistoryType
 from pathlib import Path
 from typing import Any, Generator, Tuple
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 class Client(object):
@@ -46,12 +52,11 @@ class Client(object):
         self._tokenizer = tokenizer
         self._model = model.to(device=self.device)
 
-        return
-
     @property
     def device(self) -> torch.device:
         if torch.cuda.is_available():
             device = torch.device('cuda')
+            logger.info(f'CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}')
         elif torch.backends.mps.is_available():
             device = torch.device('mps')
         else:
@@ -66,21 +71,23 @@ class Client(object):
     def model(self) -> QWenLMHeadModel:
         return self._model
 
+
     def chat(self, query: str, history: HistoryType = None) -> Tuple[str, HistoryType]:
         return self.model.chat(tokenizer=self.tokenizer, query=query, history=history)
 
+
     def chat_stream(self, query: str, history: HistoryType = None) -> Generator[str, Any, None]:
         return self.model.chat_stream(tokenizer=self.tokenizer, query=query, history=history)
-    
+
+
     def draw_bbox_on_latest_picture(self, history: HistoryType, tempdir: str) -> str | None:
         response = history[-1][1]
         image = self.tokenizer.draw_bbox_on_latest_picture(response=response, history=history)
         if image is not None:
             images_tempdir = Path(tempdir) / 'images'
             images_tempdir.mkdir(exist_ok=True, parents=True)
-            name = f'tmp-{uuid.uuid4()}.jpg'
+            name = f'img-{uuid.uuid4()}.jpg'
             filename = images_tempdir / name
             image.save(str(filename))
             return str(filename)
-        else:
-            return None
+        return None
--- shirley/webui.py
+++ shirley/webui.py
@@ -12,7 +12,7 @@ from gradio.events import Dependency
 from models.qwen_vl_chat.qwen_generation_utils import HistoryType
 from pathlib import Path
 from shirley.types import Chatbot, HistoryState, MultimodalTextbox
-from shirley.utils import getpath, isimage, parse
+from shirley.utils import getpath, isimage
 from typing import Callable, Iterator, List, Sequence, Tuple
 
 
@@ -27,7 +27,7 @@ class WebUI(object):
     def __init__(self, client: shirley.Client) -> None:
         self._client = client
         self._generating = False
-        self._blocks = self.__create_blocks()
+        self._blocks = self._create_blocks()
 
     @property
     def client(self) -> shirley.Client:
@@ -46,8 +46,50 @@ class WebUI(object):
         return self._blocks
 
 
+    def launch(self, *args, **kwargs) -> Tuple[FastAPI, str, str]:
+        self.blocks.queue(api_open=False)
+        return self.blocks.launch(*args, **kwargs)
+
+
     @staticmethod
-    def retrieve(filepath: str) -> str:
+    def _parse(text: str, remove_image_tags: bool = False) -> str:
+        lines = text.split('\n')
+        lines = [line for line in lines if line != '']
+        count = 0
+        for i, line in enumerate(lines):
+            if '```' in line:
+                count += 1
+                items = line.split('`')
+                if count % 2 == 1:
+                    lines[i] = f'<pre><code class="language-{items[-1]}">'
+                else:
+                    lines[i] = f'<br></code></pre>'
+            else:
+                if i > 0:
+                    if count % 2 == 1:
+                        line = line.replace('`', r'\`')
+                        line = line.replace('<', '&lt;')
+                        line = line.replace('>', '&gt;')
+                        line = line.replace(' ', '&nbsp;')
+                        line = line.replace('*', '&ast;')
+                        line = line.replace('_', '&lowbar;')
+                        line = line.replace('-', '&#45;')
+                        line = line.replace('.', '&#46;')
+                        line = line.replace('!', '&#33;')
+                        line = line.replace('(', '&#40;')
+                        line = line.replace(')', '&#41;')
+                        line = line.replace('$', '&#36;')
+                    lines[i] = '<br>' + line
+
+        text = ''.join(lines)
+        if remove_image_tags:
+            text = text.replace('<ref>', '').replace('</ref>', '')
+            text = re.sub(r'<box>.*?(</box>|$)', '', text)
+        return text
+
+
+    @staticmethod
+    def _load_context(filepath: str) -> str:
         if isimage(filepath):
             return f'Picture: <img>{filepath}</img>'
         elif filepath.endswith('.pdf'):
@@ -58,35 +100,35 @@ class WebUI(object):
             return ''
 
 
-    def generate(self, *args, **kwargs) -> Sequence | Iterator[Sequence]:
+    def _get_query_and_history(self, history_state: HistoryState) -> Tuple[str, HistoryType]:
+        history: HistoryType = []
+        text = ''
+        for _, (query, response) in enumerate(history_state):
+            if isinstance(query, (Tuple, List)):
+                filepath = query[0]
+                context = self._load_context(filepath=filepath)
+                text += context + '\n'
+            else:
+                text += query
+                history.append((text, response))
+                text = ''
+        return history[-1][0], history[:-1]
+
+
+    def _generate(self, *args, **kwargs) -> Sequence | Iterator[Sequence]:
         chatbot: Chatbot = args[0]
         history_state: HistoryState = args[1]
 
         self.generating = True
         logger.info(f'🙂 User: {chatbot[-1][0]}')
 
-        history: HistoryType = []
-        text = ''
-        for _, (query, response) in enumerate(history_state):
-            if isinstance(query, (Tuple, List)):
-                filepath = query[0]
-                context = self.retrieve(filepath=filepath)
-                text += context + '\n'
-            else:
-                text += query
-                history.append((text, response))
-                text = ''
-
-        query, history = history[-1][0], history[:-1]
+        query, history = self._get_query_and_history(history_state)
         full_response = ''
         for response in self.client.chat_stream(query=query, history=history):
             if not self.generating: break
-            text = parse(response)
-            text = text.replace('<ref>', '').replace('</ref>', '')
-            text = re.sub(r'<box>.*?(</box>|$)', '', text)
-            chatbot[-1] = (chatbot[-1][0], text)
+            chatbot[-1] = (chatbot[-1][0], self._parse(text=response, remove_image_tags=True))
             yield chatbot, history_state
-            full_response = parse(response)
+            full_response = self._parse(text=response)
 
         history.append((query, full_response))
         image_filepath = self.client.draw_bbox_on_latest_picture(history=history, tempdir=self.tempdir)
@@ -97,11 +139,11 @@ class WebUI(object):
         history_state[-1] = (history_state[-1][0], full_response)
 
         logger.info(f'🦈 Shirley: {full_response}')
-        yield chatbot, history_state
         self.generating = False
+        yield chatbot, history_state
 
 
-    def regenerate(self, *args, **kwargs) -> Sequence | Iterator[Sequence]:
+    def _regenerate(self, *args, **kwargs) -> Sequence | Iterator[Sequence]:
         chatbot: Chatbot = args[0]
         history_state: HistoryState = args[1]
 
@@ -119,10 +161,10 @@ class WebUI(object):
         else:
             chatbot.append((chatbot_last[0], None))
 
-        yield from self.generate(*args, **kwargs)
+        yield from self._generate(*args, **kwargs)
 
 
-    def pregenerate(self, *args, **kwargs) -> Sequence[Component]:
+    def _pregenerate(self, *args, **kwargs) -> Sequence[Component]:
         components = [
             gr.MultimodalTextbox(interactive=False),
             gr.Button(variant='secondary', interactive=False),
@@ -133,7 +175,7 @@ class WebUI(object):
         return tuple(components)
 
 
-    def postgenerate(self, *args, **kwargs) -> Sequence[Component]:
+    def _postgenerate(self, *args, **kwargs) -> Sequence[Component]:
         components = [
             gr.MultimodalTextbox(interactive=True),
             gr.Button(variant='secondary', interactive=False),
@@ -144,7 +186,7 @@ class WebUI(object):
         return tuple(components)
 
 
-    def change(self, *args, **kwargs) -> Component:
+    def _change(self, *args, **kwargs) -> Component:
         multimodal_textbox: MultimodalTextbox = args[0]
 
         text = multimodal_textbox['text']
@@ -154,7 +196,7 @@ class WebUI(object):
         return gr.Button(variant='primary', interactive=True)
 
 
-    def submit(self, *args, **kwargs) -> Sequence:
+    def _submit(self, *args, **kwargs) -> Sequence:
         chatbot: Chatbot = args[0]
         history_state: HistoryState = args[1]
         multimodal_textbox: MultimodalTextbox = args[2]
@@ -168,17 +210,17 @@ class WebUI(object):
             history_state = history_state + [((filepath,), None)]
 
         if multimodal_textbox['text'] is not None:
-            chatbot = chatbot + [(parse(multimodal_textbox['text']), None)]
+            chatbot = chatbot + [(self._parse(text=multimodal_textbox['text']), None)]
             history_state = history_state + [(multimodal_textbox['text'], None)]
 
         return chatbot, history_state, None
 
 
-    def stop(self, *args, **kwargs) -> None:
+    def _stop(self, *args, **kwargs) -> None:
         self.generating = False
 
 
-    def reset(self, *args, **kwargs) -> Sequence[Component]:
+    def _reset(self, *args, **kwargs) -> Sequence[Component]:
         components = [
             gr.MultimodalTextbox(interactive=True),
             gr.Button(variant='secondary', interactive=False),
@@ -189,7 +231,7 @@ class WebUI(object):
         return tuple(components)
 
 
-    def log(self, *args, **kwargs) -> None:
+    def _log(self, *args, **kwargs) -> None:
         chatbot: Chatbot = args[0]
         history_state: HistoryState = args[1]
 
@@ -197,7 +239,136 @@ class WebUI(object):
         logger.info(f'HistoryState: {history_state}')
 
 
-    def __create_blocks(self) -> gr.Blocks:
+    def _subscribe_generate(self, dependency: Dependency, generate_fn: Callable, *args, **kwargs) -> None:
+        chatbot: gr.Chatbot = kwargs['chatbot']
+        history_state: gr.State = kwargs['history_state']
+        multimodal_textbox: gr.MultimodalTextbox = kwargs['multimodal_textbox']
+        submit_button: gr.Button = kwargs['submit_button']
+        stop_button: gr.Button = kwargs['stop_button']
+        regenerate_button: gr.Button = kwargs['regenerate_button']
+        reset_button: gr.Button = kwargs['reset_button']
+
+        success = dependency.success(fn=lambda:None, show_api=False)
+        pregenerate = success.then(
+            fn=self._pregenerate,
+            inputs=None,
+            outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
+            show_api=False,
+        )
+        generate = pregenerate.then(
+            fn=generate_fn,
+            inputs=[chatbot, history_state],
+            outputs=[chatbot, history_state],
+            show_progress=True,
+            show_api=False,
+        )
+        postgenerate = generate.then(
+            fn=self._postgenerate,
+            inputs=None,
+            outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
+            show_api=False,
+        )
+        postgenerate.then(
+            fn=self._log,
+            inputs=[chatbot, history_state],
+            outputs=None,
+            show_api=False,
+        )
+
+
+    def _subscribe_events_dark_mode_button(self, *args, **kwargs) -> None:
+        dark_mode_button: gr.Button = kwargs['dark_mode_button']
+
+        dark_mode_button.click(
+            fn=None,
+            js='() => { document.body.classList.toggle("dark"); }',
+            show_api=False,
+        )
+
+
+    def _subscribe_events_multimodal_textbox(self, *args, **kwargs) -> None:
+        chatbot: gr.Chatbot = kwargs['chatbot']
+        history_state: gr.State = kwargs['history_state']
+        multimodal_textbox: gr.MultimodalTextbox = kwargs['multimodal_textbox']
+        submit_button: gr.Button = kwargs['submit_button']
+
+        multimodal_textbox.change(
+            fn=self._change,
+            inputs=[multimodal_textbox],
+            outputs=[submit_button],
+            show_api=False,
+        )
+
+        submit = multimodal_textbox.submit(
+            fn=self._submit,
+            inputs=[chatbot, history_state, multimodal_textbox],
+            outputs=[chatbot, history_state, multimodal_textbox],
+            show_api=False,
+        )
+        self._subscribe_generate(dependency=submit, generate_fn=self._generate, *args, **kwargs)
+
+
+    def _subscribe_events_submit_button(self, *args, **kwargs) -> None:
+        chatbot: gr.Chatbot = kwargs['chatbot']
+        history_state: gr.State = kwargs['history_state']
+        multimodal_textbox: gr.MultimodalTextbox = kwargs['multimodal_textbox']
+        submit_button: gr.Button = kwargs['submit_button']
+
+        click = submit_button.click(
+            fn=self._submit,
+            inputs=[chatbot, history_state, multimodal_textbox],
+            outputs=[chatbot, history_state, multimodal_textbox],
+            show_api=False,
+        )
+        self._subscribe_generate(dependency=click, generate_fn=self._generate, *args, **kwargs)
+
+
+    def _subscribe_events_stop_button(self, *args, **kwargs) -> None:
+        stop_button: gr.Button = kwargs['stop_button']
+
+        stop_button.click(fn=self._stop, show_api=False)
+
+
+    def _subscribe_events_regenerate_button(self, *args, **kwargs) -> None:
+        regenerate_button: gr.Button = kwargs['regenerate_button']
+
+        click = regenerate_button.click(fn=lambda:None, show_api=False)
+        self._subscribe_generate(dependency=click, generate_fn=self._regenerate, *args, **kwargs)
+
+
+    def _subscribe_events_reset_button(self, *args, **kwargs) -> None:
+        chatbot: gr.Chatbot = kwargs['chatbot']
+        history_state: gr.State = kwargs['history_state']
+        multimodal_textbox: gr.MultimodalTextbox = kwargs['multimodal_textbox']
+        submit_button: gr.Button = kwargs['submit_button']
+        stop_button: gr.Button = kwargs['stop_button']
+        regenerate_button: gr.Button = kwargs['regenerate_button']
+        reset_button: gr.Button = kwargs['reset_button']
+
+        reset_button_click = reset_button.click(
+            fn=lambda: ([], [], None),
+            inputs=None,
+            outputs=[chatbot, history_state, multimodal_textbox],
+            show_api=False,
+        )
+        reset_button_click.then(
+            fn=self._reset,
+            inputs=None,
+            outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
+            show_api=False,
+        )
+
+
+    def _subscribe_events(self, *args, **kwargs) -> None:
+        self._subscribe_events_dark_mode_button(*args, **kwargs)
+        self._subscribe_events_multimodal_textbox(*args, **kwargs)
+        self._subscribe_events_submit_button(*args, **kwargs)
+        self._subscribe_events_stop_button(*args, **kwargs)
+        self._subscribe_events_regenerate_button(*args, **kwargs)
+        self._subscribe_events_reset_button(*args, **kwargs)
+
+
+    def _create_blocks(self, *args, **kwargs) -> gr.Blocks:
         with gr.Blocks(theme=gr.themes.Default(), title='Shirley WebUI', fill_width=True) as blocks:
             with gr.Row():
                 with gr.Column(scale=1):
@@ -226,11 +397,11 @@ class WebUI(object):
                     chatbot = gr.Chatbot(
                         type='tuples',
                         label='🦈 Shirley',
-                        height='80vh',
+                        height='70vh',
                         show_copy_button=True,
                         avatar_images=(None, getpath('./static/apple-touch-icon.png')),
                     )
-                    history = gr.State(value=[])
+                    history_state = gr.State(value=[])
                     multimodal_textbox = gr.MultimodalTextbox(
                         placeholder='✏️ Enter text or upload file… (输入文字或者上传文件…)',
                         show_label=False,
@@ -244,99 +415,30 @@ class WebUI(object):
                         regenerate_button = gr.Button(value='🤔️ Regenerate (重新生成)', interactive=False)
                         reset_button = gr.Button(value='🧹 Reset (重置对话)', interactive=False)
 
-            def subscribe_events_generate(dependency: Dependency, generate_fn: Callable = self.generate) -> None:
-                dependency.success(
-                    fn=self.pregenerate,
-                    inputs=None,
-                    outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
-                    show_api=False,
-                ).then(
-                    fn=generate_fn,
-                    inputs=[chatbot, history],
-                    outputs=[chatbot, history],
-                    show_progress=True,
-                    show_api=False,
-                ).then(
-                    fn=self.postgenerate,
-                    inputs=None,
-                    outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
-                    show_api=False,
-                ).then(
-                    fn=self.log,
-                    inputs=[chatbot, history],
-                    outputs=None,
-                    show_api=False,
-                )
-
-            # dark mode button
-            dark_mode_button.click(
-                fn=None,
-                js='() => { document.body.classList.toggle("dark"); }',
-                show_api=False,
-            )
-            
-            # multimodal textbox
-            multimodal_textbox.change(
-                fn=self.change,
-                inputs=[multimodal_textbox],
-                outputs=[submit_button],
-                show_api=False,
-            )
-            multimodal_textbox_submit = multimodal_textbox.submit(
-                fn=self.submit,
-                inputs=[chatbot, history, multimodal_textbox],
-                outputs=[chatbot, history, multimodal_textbox],
-                show_api=False,
-            )
-            subscribe_events_generate(dependency=multimodal_textbox_submit)
-
-            # submit button
-            submit_button_click = submit_button.click(
-                fn=self.submit,
-                inputs=[chatbot, history, multimodal_textbox],
-                outputs=[chatbot, history, multimodal_textbox],
-                show_api=False,
-            )
-            subscribe_events_generate(dependency=submit_button_click)
-
-            # stop button
-            stop_button.click(fn=self.stop, show_api=False)
-
-            # regenerate button
-            regenerate_button_click = regenerate_button.click(fn=lambda:None, show_api=False)
-            subscribe_events_generate(dependency=regenerate_button_click, generate_fn=self.regenerate)
-
-            # reset button
-            reset_button.click(
-                fn=lambda: ([], [], None),
-                inputs=None,
-                outputs=[chatbot, history, multimodal_textbox],
-                show_api=False,
-            ).then(
-                fn=self.reset,
-                inputs=None,
-                outputs=[multimodal_textbox, submit_button, stop_button, regenerate_button, reset_button],
-                show_api=False,
+            self._subscribe_events(
+                dark_mode_button=dark_mode_button,
+                chatbot=chatbot,
+                history_state=history_state,
+                multimodal_textbox=multimodal_textbox,
+                submit_button=submit_button,
+                stop_button=stop_button,
+                regenerate_button=regenerate_button,
+                reset_button=reset_button,
             )
 
             return blocks
 
 
-    def launch(self) -> Tuple[FastAPI, str, str]:
-        self.blocks.queue()
-        return self.blocks.launch(
-            share=False,
-            inbrowser=False,
-            server_port=8000,
-            server_name='127.0.0.1',
-            favicon_path=getpath('./static/favicon.ico'),
-        )
-
-
 def main() -> None:
     client = shirley.Client(pretrained_model_path=getpath('./models/qwen_vl_chat'))
     webui = WebUI(client=client)
-    webui.launch()
+    webui.launch(
+        share=False,
+        inbrowser=False,
+        server_port=8000,
+        server_name='127.0.0.1',
+        favicon_path=getpath('./static/favicon.ico'),
+    )
 
 
 if __name__ == '__main__':

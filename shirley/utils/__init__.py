349c0a7cd5da12fc39a1cde233f74526e92026e7
luojiahai <ljiahai@hotmail.com>
2024-08-09 01:26:57 +1000

    Update

--- shirley/utils/__init__.py
+++ shirley/utils/__init__.py
@@ -1,1 +1,15 @@
-from shirley.utils.utils import *
+import os
+from PIL import Image
+
+
+def getpath(filepath: str) -> str:
+    return os.path.abspath(os.path.expanduser(filepath))
+
+
+def isimage(filepath: str) -> bool:
+    try:
+        with Image.open(filepath) as img:
+            img.verify()
+            return True
+    except (IOError, SyntaxError):
+        return False

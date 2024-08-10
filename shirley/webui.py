f7c6ad9fb1461261c872eb25080513e0b505ea4c
luojiahai <ljiahai@hotmail.com>
2024-08-10 16:55:37 +1000

    Update

--- shirley/webui.py
+++ shirley/webui.py
@@ -1,58 +1,30 @@
-import fastapi
 import gradio as gr
-import logging
 import shirley as sh
-import sys
-from typing import Tuple
-
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-
-class WebUI(object):
-
-    def __init__(self) -> None:
-        self._blocks = self._make_blocks()
-
-
-    @property
-    def blocks(self) -> gr.Blocks:
-        return self._blocks
-
-
-    def launch(self, *args, **kwargs) -> Tuple[fastapi.FastAPI, str, str]:
-        self.blocks.queue()
-        return self.blocks.launch(*args, **kwargs)
-
-
-    def _make_blocks(self, *args, **kwargs) -> gr.Blocks:
-        avatar_images=(
-            sh.utils.getpath('./static/images/grinning-face.png'),
-            sh.utils.getpath('./static/images/shark.png'),
-        )
-
-        with gr.Blocks(
-            theme=gr.themes.Default(
-                primary_hue=gr.themes.colors.cyan,
-                secondary_hue=gr.themes.colors.sky,
-                radius_size=gr.themes.sizes.radius_none,
-            ),
-            title='Shirley WebUI',
-            css=sh.utils.getpath('./static/css/custom.css'),
-        ) as blocks:
-            sh.interfaces.Header()
-            with gr.Tab('✨ Chat (聊天/唠嗑)'):
-                sh.interfaces.Chat(avatar_images=avatar_images)
-            with gr.Tab('💬 Text-To-Speech (文字转语音)'):
-                sh.interfaces.TextToSpeech()
-            sh.interfaces.Footer()
-            return blocks
 
 
 def main() -> None:
-    webui = WebUI()
-    webui.launch(
+    avatar_images=(
+        sh.utils.getpath('./static/images/grinning-face.png'),
+        sh.utils.getpath('./static/images/shark.png'),
+    )
+
+    with gr.Blocks(
+        theme=gr.themes.Default(
+            primary_hue=gr.themes.colors.cyan,
+            secondary_hue=gr.themes.colors.sky,
+            radius_size=gr.themes.sizes.radius_none,
+        ),
+        title='Shirley WebUI',
+        css=sh.utils.getpath('./static/css/custom.css'),
+    ) as blocks:
+        sh.interfaces.Header()
+        with gr.Tab('✨ Chat (聊天/唠嗑)'):
+            sh.interfaces.Chat(avatar_images=avatar_images)
+        with gr.Tab('💬 Text-To-Speech (文字转语音)'):
+            sh.interfaces.TextToSpeech()
+        sh.interfaces.Footer()
+
+    blocks.queue().launch(
         server_name='127.0.0.1',
         server_port=8000,
         favicon_path=sh.utils.getpath('./static/favicon.ico'),
